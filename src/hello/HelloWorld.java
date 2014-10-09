400950779ccb35bdd025c76a3f4a5d06d3072611
kyo <604079771@qq.com>
2014-10-09 10:43:49 +0800

    First Commit！

---
+++ src/hello/HelloWorld.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class HelloWorld {
+		
+	void print(){
+			System.out.print("hello");
+			}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("hello world");
+		//System.out.println(new Date());
+		//System.getProperties().list(System.out);
+		//System.out.println(System.getProperty("user.name"));
+		//System.out.println(System.getProperty("java.library.path")));
+		new HelloWorld().print();
+	}
+} 
