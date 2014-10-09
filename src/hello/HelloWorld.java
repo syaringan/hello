5774f4ad79881ce4643eda3def1748c8f930e022
kyo <604079771@qq.com>
2014-10-09 11:00:38 +0800

    test

--- src/hello/HelloWorld.java
+++ src/hello/HelloWorld.java
@@ -7,7 +7,7 @@ public class HelloWorld {
 			}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//System.out.println("hello world");
+		System.out.println("hello world");
 		//System.out.println(new Date());
 		//System.getProperties().list(System.out);
 		//System.out.println(System.getProperty("user.name"));
